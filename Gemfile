ac5b8e9072d7e3a9e8af988e1bea4179bacc6968
Marsboy02 <marsboy0619@gmail.com>
2023-11-02 11:28:25 +0900

    test: 테스트 라이브러리 및 코드 추가

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 gem "redis", ">= 4.0.1"
 
+# test
+gem 'rails-controller-testing'
+
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'index action returns successful response' do
+    get '/'
+    assert_response :success
+  end
+
+  test 'index action assigns website statuses' do
+    get '/'
+    assert assigns(:website_statuses).is_a?(Hash)
+  end
+
+  test 'index action assigns correct status codes for websites' do
+    websites = [
+      { name: '메인', url: 'https://uos.ac.kr' },
+      { name: '와이즈', url: 'https://wise.uos.ac.kr' },
+    # Add more websites as needed
+    ]
+
+    get '/'
+
+    # Replace the following line with your actual website status code checks
+    # Example: assert_equal 200, assigns(:website_statuses)['메인']
+  end
 end
