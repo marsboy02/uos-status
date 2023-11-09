59b1d90f7b4eb59eeeda8db285924633d3b8814c
Marsboy02 <marsboy0619@gmail.com>
2023-11-09 18:30:02 +0900

    fix: https가 아닌 http 연결을 시도하는 사이트 분리

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,13 +12,13 @@ class HomeController < ApplicationController
       { name: 'JBS', url: 'https://jbs.uos.ac.kr', description: '신문을 제작하는 학생자치의 홈페이지입니다.' },
       { name: '대학보건소', url: 'https://health.uos.ac.kr', description: '보건소 페이지입니다.' },
       { name: '장학금', url: 'https://scholarship.uos.ac.kr', description: '장학금 페이지입니다.' },
-      { name: '교수학습', url: 'https://ctl.uos.ac.kr', description: '수업 관련된 지원을 제공하는 페이지입니다.'},
+      { name: '현장실습', url: 'https://field.uos.ac.kr', description: '현장실습 페이지입니다.' },
       { name: '기숙사', url: 'https://dormitory.uos.ac.kr', description: '기숙사 페이지입니다.' },
       { name: '체육관', url: 'https://physical.uos.ac.kr', description: '체육 시설 예약 등을 제공하는 페이지입니다.'},
       { name: '발전기금', url: 'https://fund.uos.ac.kr', description: '발전기금 후원 페이지입니다.'},
       { name: 'Times', url: 'https://times.uos.ac.kr', description: '영문으로 만든 신문을 제공하는 페이지입니다.' },
       { name: 'Press', url: 'https://press.uos.ac.kr', description: '서울시립대학교 신문 홈페이지입니다.' },
-      # { name: '현장실습', url: 'https://field.uos.ac.kr'},
+
       # { name: 'HRC', url: 'https://hrc.uos.ac.kr' },
       # { name: 'USI', url: 'https://usi.uos.ac.kr' },
       # { name: 'CIS', url: 'https://cis.uos.ac.kr' },
@@ -28,6 +28,8 @@ class HomeController < ApplicationController
       # { name: '100th', url: 'https://100th.uos.ac.kr' },
       # { name: 'Abeek', url: 'https://abeek.uos.ac.kr' },
       # { name: '행복인문학', url: 'https://aph.uos.ac.kr'},
+      # http
+      # { name: '교수학습', url: 'http://ctl.uos.ac.kr'},
       # { name: '창업지원단', url: 'http://startup.uos.ac.kr'},
 
     ]
