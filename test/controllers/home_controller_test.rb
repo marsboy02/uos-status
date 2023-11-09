323e2b57f3bf1e013cf72cdd7282740058326d7a
Marsboy02 <marsboy0619@gmail.com>
2023-11-09 18:16:33 +0900

    test: 테스트 코드 관련 파일 수정 및 ci 스크립트 수정

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,21 +6,13 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'index action renders correct template' do
+    get '/'
+    assert_template :index
+  end
+
   test 'index action assigns website statuses' do
     get '/'
     assert assigns(:website_statuses).is_a?(Hash)
-  end
-
-  test 'index action assigns correct status codes for websites' do
-    websites = [
-      { name: '메인', url: 'https://uos.ac.kr' },
-      { name: '와이즈', url: 'https://wise.uos.ac.kr' },
-    # Add more websites as needed
-    ]
-
-    get '/'
-
-    # Replace the following line with your actual website status code checks
-    # Example: assert_equal 200, assigns(:website_statuses)['메인']
   end
 end
