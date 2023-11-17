56bd2e6ce8f7ec671800a672e666bac380c783a5
Marsboy02 <marsboy0619@gmail.com>
2023-11-17 11:59:48 +0900

    fix: 프로파일링하는 웹사이트 수정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
       { name: '메인', url: 'https://uos.ac.kr', description: '메인 페이지입니다.' },
       { name: '와이즈', url: 'https://wise.uos.ac.kr', description: '행정 정보를 처리하는 페이지입니다.' },
       { name: '포탈', url: 'https://portal.uos.ac.kr', description: '로그인이 필요한 메인 페이지입니다.' },
-      { name: 'UOS Story', url: 'https://uostory.uos.ac.kr', description: '다양한 유틸리티를 제공합니다.' },
+      # { name: 'UOS Story', url: 'https://uostory.uos.ac.kr', description: '다양한 유틸리티를 제공합니다.' },
       { name: '도서관', url: 'https://library.uos.ac.kr', description: '도서관 홈페이지입니다.' },
       { name: 'JBS', url: 'https://jbs.uos.ac.kr', description: '신문을 제작하는 학생자치의 홈페이지입니다.' },
       { name: '대학보건소', url: 'https://health.uos.ac.kr', description: '보건소 페이지입니다.' },
@@ -15,7 +15,7 @@ class HomeController < ApplicationController
       { name: '기숙사', url: 'https://dormitory.uos.ac.kr', description: '기숙사 페이지입니다.' },
       { name: '체육관', url: 'https://physical.uos.ac.kr', description: '체육 시설 예약 등을 제공하는 페이지입니다.'},
       { name: '발전기금', url: 'https://fund.uos.ac.kr', description: '발전기금 후원 페이지입니다.'},
-      { name: 'Times', url: 'https://times.uos.ac.kr', description: '영문으로 만든 신문을 제공하는 페이지입니다.' },
+      # { name: 'Times', url: 'https://times.uos.ac.kr', description: '영문으로 만든 신문을 제공하는 페이지입니다.' },
       # { name: '현장실습', url: 'https://field.uos.ac.kr', description: '현장실습 페이지입니다.' },
       # { name: 'Press', url: 'https://press.uos.ac.kr', description: '서울시립대학교 신문 홈페이지입니다.' },
 
@@ -36,6 +36,7 @@ class HomeController < ApplicationController
 
     @website_statuses = {}
     websites.each do |website|
+      puts(website[:name])
       uri = URI.parse(website[:url])
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = (uri.scheme == 'https')
