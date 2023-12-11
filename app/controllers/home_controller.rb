08cd1303ac09d4714f4264874e1cde93eb2a2921
Marsboy02 <marsboy0619@gmail.com>
2023-12-12 00:14:58 +0900

    fix: http 트래픽을 이상하게 받는 사이트 제거

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,18 +17,19 @@ class HomeController < ApplicationController
       { name: '체육관', url: 'https://physical.uos.ac.kr', description: '체육 시설 예약 등을 제공하는 페이지입니다.'},
       { name: '발전기금', url: 'https://fund.uos.ac.kr', description: '발전기금 후원 페이지입니다.'},
       { name: 'Times', url: 'https://times.uos.ac.kr', description: '영문으로 만든 신문을 제공하는 페이지입니다.' },
-      { name: '현장실습', url: 'https://field.uos.ac.kr', description: '현장실습 페이지입니다.' },
+      # { name: '현장실습', url: 'https://field.uos.ac.kr', description: '현장실습 페이지입니다.' },
       { name: 'Press', url: 'https://press.uos.ac.kr', description: '서울시립대학교 신문 홈페이지입니다.' },
       { name: '전산정보원', url: 'https://cis.uos.ac.kr', description: "다양한 IT 서비스를 지원해주는 페이지입니다." },
+      { name: '국제교육원', url: 'https://oia.uos.ac.kr' , description: '외국인 학생들을 대상으로 다양한 서비스를 제공합니다' },
       # { name: '인권센터', url: 'https://hrc.uos.ac.kr' },
       # { name: '도시과학연구원', url: 'https://usi.uos.ac.kr' },
-      # { name: '국제교육원', url: 'https://oia.uos.ac.kr' },
-      # { name: 'ICE', url: 'https://ice.uos.ac.kr' },
-      # { name: 'CRF', url: 'https://crf.uos.ac.kr' },
+      # { name: '공동기기센터', url: 'https://crf.uos.ac.kr' },
       # { name: '100th', url: 'https://100th.uos.ac.kr' },
       # { name: 'Abeek', url: 'https://abeek.uos.ac.kr' },
       # { name: '행복인문학', url: 'https://aph.uos.ac.kr'},
+      #
       # http
+      # { name: '평생교육원', url: 'https://ice.uos.ac.kr' },
       # { name: '교수학습', url: 'http://ctl.uos.ac.kr'},
       # { name: '창업지원단', url: 'http://startup.uos.ac.kr'},
     ]
